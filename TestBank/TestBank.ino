79d0a380f2062647a89ea196278550cf15b9222f
Javier Aristóteles Prieto Flores <javierprieto621@gmail.com>
2025-01-23 16:57:15 -0600

    Update TestBank.ino

--- TestBank/TestBank.ino
+++ TestBank/TestBank.ino
@@ -1,21 +1,35 @@
 #include "ADCSetup.h"
 #include "SDLogger.h"
 
+#define SD_CS_PIN 10
+#define SD_close_button 3
+
+bool Button_state = 0; 
+float N = 0;
+
 void setup() {
   setupADC();
-  while(!setupSD(4)){
+  pinMode(SD_close_button, INPUT);
+  while(!setupSD(SD_CS_PIN)){
     Serial.println("Retrying...");
   }
   initFile();
 }
 
 void loop() {
+  Button_state = digitalRead(SD_close_button);
   if (handleConversion() == true && adcMeasurement > 50)
   {
-    Serial.print("Lectura ADC:\t");
-    logToSD(String(adcMeasurement), true);
-  }
-
+    Serial.print("Newtons:\t");
+    N = WEIGHT(adcMeasurement) * 9.81;
+    logToSD(String(N), true);
+  } 
+    
+  if (Button_state == 1)
+  {  
+    closeSD();
+  } 
+  
   // delay 10mS
   //delay(10);
 }
